2ae7656521136da8829d722a702427fd17d567d1
Xiaoyan Wu <xiaoyan.wu@cortlandglobal.com>
2017-06-21 16:59:43 +0800

    Add project files.

---
+++ ConsoleApplication14/Mode.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace ConsoleApplication14
+{
+    //public class BloggingContext : DbContext
+    //{
+    //    public DbSet<Blog> Blogs { get; set; }
+    //    public DbSet<Post> Posts { get; set; }
+    //}
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext(): base("name=DBConnectionString") { }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<TAT> Tats { get; set; }
+        public DbSet<AB> ABs { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> TbClass { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Database.SetInitializer<BloggingContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class TAT
+    {
+        [Key]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class AB
+    {
+        [Key]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Product
+    {
+        [Key]
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    [Table("TbClass")]
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ConsoleApplication14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace ConsoleApplication14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while (true)
+            {
+                //var input = Console.ReadLine();
+                //if (input == "1")DropCreateDatabaseIfModelChanges
+                //{
+                //    Database.SetInitializer<BloggingContext>(new System.Data.Entity.CreateDatabaseIfNotExists<BloggingContext>());
+                //}
+                Database.SetInitializer<BloggingContext>(new System.Data.Entity.DropCreateDatabaseIfModelChanges<BloggingContext>());
+                using (var db = new BloggingContext())
+                {
+                    var name = "123";
+
+                    var blog = new Blog { Name = name };
+                    db.Blogs.Add(blog);
+                    db.SaveChanges();
+
+                    var tat = new Product { Name = "a"+DateTime.Now.Second, Age = 19 };
+                    db.Products.Add(tat);
+                    db.SaveChanges();
+                    // Display all Blogs from the database 
+                    var query = from b in db.Blogs
+                                orderby b.Name
+                                select b;
+
+                    Console.WriteLine("All blogs in the database:");
+                    foreach (var item in query)
+                    {
+                        Console.WriteLine(item.Name);
+                    }
+
+                    Console.WriteLine("Press any key to exit...");
+                    // Console.ReadKey();
+
+                }
+            }
+           
+           
+               
+        }
+    }
+}
